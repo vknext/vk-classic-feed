773b0ee82fcb873340c8a18b0c87e13b1fe3178d
awers24 <83694881+awers24@users.noreply.github.com>
2025-05-05 12:17:48 +0300

    Фикс конфликта между vkn и vcf

--- src/listeners/onChangeVK.ts
+++ src/listeners/onChangeVK.ts
@@ -1,33 +1,10 @@
+import watchGlobalProperty from '@vknext/shared/vkcom/globalVars/utils/watchGlobalProperty';
 import type { VK } from 'src/global';
 import InteractionListener from 'src/interactions/utils/InteractionListener';
 import waitVK from 'src/lib/waitVK';
 
 type CallbackFunc = (vk: VK) => void;
 const interaction = new InteractionListener<CallbackFunc>();
-
-const hookVK = async () => {
-	await waitVK();
-
-	let vkValue: VK = window.vk;
-
-	Object.defineProperty(window, 'vk', {
-		get: () => vkValue,
-		set: (newVk: VK) => {
-			vkValue = newVk;
-
-			for (const callback of interaction.listeners) {
-				try {
-					callback(vkValue);
-				} catch (e) {
-					console.error(e);
-				}
-			}
-
-			return true;
-		},
-		configurable: true,
-	});
-};
 
 let inited = false;
 const onAddNewCallback = async (callback: CallbackFunc) => {
@@ -36,7 +13,15 @@ const onAddNewCallback = async (callback: CallbackFunc) => {
 	} else {
 		inited = true;
 
-		await hookVK();
+		watchGlobalProperty('vk', (vkValue) => {
+			for (const callback of interaction.listeners) {
+				try {
+					callback(vkValue);
+				} catch (e) {
+					console.error(e);
+				}
+			}
+		});
 	}
 
 	callback(window.vk);
